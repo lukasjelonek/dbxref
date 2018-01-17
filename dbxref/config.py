d64ebcc68dd60fed46425c82d2f9ac99fc2b5212
Lukas Jelonek <lukas.jelonek@computational.bio.uni-giessen.de>
2018-01-17 15:34:27 +0100

    Refactor project such that it can be build and deployed via pip

--- dbxref/config.py
+++ dbxref/config.py
@@ -1,15 +1,11 @@
-def get_install_location():
-    """Finds the location directory of the tool"""
-    import os
-    script_path = os.path.realpath(__file__)
-    script_dir = os.path.dirname(script_path)
-    install_dir = os.path.dirname(script_dir)
-    return install_dir
+def get_providers_path():
+    import pkg_resources
+    return pkg_resources.resource_filename('dbxref', 'providers.yaml')
 
 def load_providers():
     import yaml
     data = []
-    with open(get_install_location() + '/providers.yaml') as data_file:
+    with open(get_providers_path()) as data_file:
         data = yaml.load(data_file)
     return index_providers(data)
 
