bb7067394fbf1dd9c2e809ad5d19950f4e2592fb
Lukas Jelonek <lukas.jelonek@computational.bio.uni-giessen.de>
2019-04-24 13:50:36 +0000

    Fix error in test

--- tests/test_retriever.py
+++ tests/test_retriever.py
@@ -4,7 +4,7 @@ from dbxref import retriever, resolver
 class TestDbxrefResolve(unittest.TestCase):
 
     def test_different_case_database_prefix(self):
-        entries = resolver.convert_to_dbxrefs(['PFAM:PF00002','Pfam:PF00002','pfam:PF00002', 'EC:2.7.7.1'])
+        entries = resolver.convert_to_dbxrefs(['PFAM:PF00002','Pfam:PF00002','pfam:PF00002'])
         documents = retriever.retrieve(entries)
         for d in documents:
           with self.subTest(d=d):
