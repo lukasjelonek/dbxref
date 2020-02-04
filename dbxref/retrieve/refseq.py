515929b4e0c581d07ee43999c0e2c26a8e4dc653
Jan Keller <jkeller@cb-stud-pc07.desktop>
2020-02-04 16:21:24 +0100

    Retriever for RefSeq, prototype

---
+++ dbxref/retrieve/refseq.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import dbxref.resolver
+import requests
+import logging
+import json
+import argparse
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    """main()method for script usage"""
+
+    def retrieve():
+        return
+
+
+if __name__ == "__main__":
+    main()
