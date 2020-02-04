ed016b1d17f4578acf2d8de6201ec36da9382274
Jan Keller <jkeller@cb-stud-pc07.desktop>
2020-02-04 14:13:55 +0100

    Function name correction

--- tests/test_kegg.py
+++ tests/test_kegg.py
@@ -8,8 +8,9 @@ class TestKegg(unittest.TestCase):
         documents = kegg.retrieve([{"db": "KEGG", "id": "K00121"}], basics=True, brite=True, pathway=True,
                                   dbxrefs_links=True, formula=True, reaction=True, genes=True, motif=True,
                                   orthology=True, reference=True)
+        self.assertTrue(documents)
 
-    def test_brite_output_1(self):
+    def test_brite_output(self):
         # Test parsing and saving of a graph(v,e) in an adjacency list. Tree with one root and one continuous branch
         brite_example_1 = [["BRITE       Root1"],
                            ["             branch1"],
