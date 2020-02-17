0547ab60d9d97d7b7688b7687fcae94d1bc59724
Jan Keller <jkeller@cb-stud-pc07.desktop>
2020-02-17 14:03:31 +0100

    Unittest for HAMAP retriever hamap.py

---
+++ tests/test_hamap.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import unittest
+from dbxref.retrieve import hamap
+
+
+class TestHAMAP(unittest.TestCase):
+
+    def test_output(self):
+        """test if HAMAP retriever gives any output, ergo functions in any way"""
+        documents = hamap.retrieve({"db": "HAMAP", "id": "HM:MF_00607"}, matrix=True)
+
+        self.assertTrue(documents)
+
+
+if __name__ == "__main__":
+    unittest.main()
