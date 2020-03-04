c550726a4efd1fd0f2f8c1f3117311bf3a1e6a3b
Lukas Jelonek <lukas.jelonek@computational.bio.uni-giessen.de>
2020-03-04 10:34:10 +0100

    Fix failing tests

--- tests/test_rfam.py
+++ tests/test_rfam.py
@@ -5,7 +5,7 @@ from dbxref.retrieve import rfam
 class TestRfam(unittest.TestCase):
 
     def test_output(self):
-        documents = rfam.retrieve({"db": "Rfam", "id": "RF03094"}, basics=True, references=True)
+        documents = rfam.retrieve([{"db": "Rfam", "id": "RF03094"}], basics=True, references=True)
         self.assertTrue(documents)
 
 
